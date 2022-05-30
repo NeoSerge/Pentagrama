514dd7c81d558adf70d671c6943d837769463d62
Ricardo <richarsam.100@gmail.com>
2022-05-30 04:03:25 -0500

    multiplayer parte 1 de 5

---
+++ Discos_VR/Assets/Scripts/Network/NetworkGamePlayerDisco.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+namespace DiscoVR
+{
+    public class NetworkGamePlayerDisco : NetworkBehaviour
+    {
+        [SyncVar]
+        public string DisplayName = "Loading ...";
+
+        private NetworkManagerDisco room;
+        private NetworkManagerDisco Room
+        {
+            get
+            {
+                if (room != null) { return room; }
+                return room = NetworkManager.singleton as NetworkManagerDisco;
+            }
+        }
+
+        public override void OnStartClient()
+        {
+            if (Room.dontDestroyOnLoad) { DontDestroyOnLoad(gameObject); }
+
+            Room.GamePlayers.Add(this);
+        }
+        
+        public override void OnStopClient()
+        {
+            Room.GamePlayers.Remove(this);
+        }
+
+    }
+}
